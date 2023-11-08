355df0fb38198a054e3f2bf23a9b9b4c4eeecca7
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-11-08 11:12:46 +0100

    view board

---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    final static int WIDTH = 8;
+    final static int HEIGHT = 8;
+    final private List<Figure> figures = new ArrayList<>();
+
+    public String composeBoardView() {
+        String board = "";
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+
+                boolean isPlayer = false;
+                for (Figure figure : figures) {
+                    if (figure.getY() == y && figure.getX() == x) {
+                        board = board + figure.getName();
+                        isPlayer = true;
+                    }
+                }
+                if (!isPlayer) {
+                    board += " * ";
+                }
+            }
+            board = board + "\n";
+        }
+        return board;
+    }
+}
---
+++ src/main/java/org/example/Figure.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Figure {
+
+
+    private String name;
+
+    public Figure(String name) {
+        this.name = name;
+    }
+
+
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+}
